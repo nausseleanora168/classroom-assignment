8ef503adea672ba03dacf6fa0f5c8c62c7587211
nausseleanora168 <nausseleanora168@gmail.com>
2025-03-09 07:17:35 +0000

    Add 4.go

---
+++ src/4.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "John"
+	age := 30
+	city := "New York"
+
+	fmt.Println("Hello, my name is", name, "and I am", age, "years old.")
+	fmt.Println("I live in", city)
+}
